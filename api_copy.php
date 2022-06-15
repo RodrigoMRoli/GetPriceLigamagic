f1b221bf515f3306c5a20da4d15b3a9e43a2f8af
Rodrigo Roli <rodrigo.monteiro.roli@gmail.com>
2022-06-15 12:33:12 -0300

    add readme, var.php, mysql_connect.php and change api_copy.php

---
+++ api_copy.php
@@ -0,0 +1,80 @@
+<?php
+include "mysql_connect.php";
+include "var.php";
+
+
+function tipoSQL($tipoTabela, $nome_colecao) {
+    switch ($tipoTabela) {
+        case 0: // ordem por ID
+            $sql = "SELECT * FROM `$nome_colecao` ORDER BY `$nome_colecao`.`COL 1` ASC ";
+            break;
+        case 1: // ordem alfabetica PT-BR
+            $sql = "SELECT * FROM `$nome_colecao` ORDER BY `$nome_colecao`.`COL 4` ASC ";
+            break;
+        case 2: // ordem alfabetica EN
+            $sql = "SELECT * FROM `$nome_colecao` ORDER BY `$nome_colecao`.`COL 5` ASC ";
+            break;
+        case 3: // ordem por Raridade
+            $sql = "SELECT * FROM `$nome_colecao` ORDER BY `$nome_colecao`.`COL 9` ASC ";
+            break;
+        case 4: // ordem por Cor
+            $sql = "SELECT * FROM `$nome_colecao` ORDER BY `$nome_colecao`.`COL 10` ASC ";
+            break;
+        case 5: // ordem por Preco
+            $sql = "SELECT * FROM `$nome_colecao` ORDER BY `$nome_colecao`.`COL 10` ASC ";
+            break;
+    }
+    return $sql;
+}
+
+function getPrecoBanco() {
+    $contPage = 0; $numTotalCartas = 0; $numPage = 1;
+    $sql = tipoSQL(1, 'ixalan');
+    $rs_cartas = mysqli_query($GLOBALS["conn"],$sql);
+    $url = $GLOBALS["url"];
+    $conteudoLigaMagic = file_get_contents($url);
+    foreach($rs_cartas as $arrNomeCartas){
+        $contPage++;
+        $numTotalCartas++;
+        $nomeDaCartaBR = $arrNomeCartas['COL 4'];
+        $nome_colecao = $arrNomeCartas['COL 2'];
+        $conteudoParteUm = "<td class='col-pcompra'>R$ ";
+        $conteudoParteDois = "</td>";
+        $parteUm = explode($conteudoParteUm, $conteudoLigaMagic );
+        $parteDois = explode($conteudoParteDois, $parteUm[$contPage]);
+        $sql2 = "UPDATE `$nome_colecao` SET `COL 14`='$parteDois[0]' WHERE `COL 4`= '$nomeDaCartaBR' ";
+        $rs2 = mysqli_query($GLOBALS["conn"],$sql2);
+        $qtComum = 0; $qtIncomum = 0; $qtRaro = 0; $qtMitico = 0;
+        switch ($arrNomeCartas['COL 9']){
+            case "C":
+                $qtComum++;
+                break;
+            case "U":
+                $qtIncomum++;
+                break;
+            case "R":
+                $qtRaro++;
+                break;
+            case "M":
+                $qtMitico++;
+                break;
+        }
+        echo "            <tr> \n";
+        echo "                <td>"; echo $arrNomeCartas["id"]; echo "</td>  \n";
+        echo "                <td>"; echo $arrNomeCartas["COL 4"]; echo "</td>  \n";
+        echo "                <td>"; echo $arrNomeCartas["COL 5"]; echo "</td> \n";
+        echo "                <td>"; echo $arrNomeCartas["COL 9"]; echo "</td> \n";
+        echo "                <td>"; echo $arrNomeCartas["COL 10"]; echo "</td> \n";
+        echo "                <td>R$ "; echo $arrNomeCartas["COL 14"]; echo "</td> \n";
+        echo "            </tr>\n";
+
+        if ($contPage % 80 == 0){
+            $numPage++;
+            $contPage = 0;
+            $url = $url;
+            $conteudoLigaMagic = file_get_contents($url);
+        }
+    }
+}
+
+?>
---
+++ var.php
@@ -0,0 +1,10 @@
+<?php
+define("pl", "<br>");
+$numTotalCartas = 0;
+$contPage = 0;
+$numPage = 1;
+$id_colecao = 6197;
+$nome_colecao = "ixalan";
+$nome_colecao_red = "xln";
+$url = "https://www.ligamagic.com.br/?view=colecao/colecao&id=$id_colecao&page=$numPage&orderBy=1&modoPrecos=5";
+?>
